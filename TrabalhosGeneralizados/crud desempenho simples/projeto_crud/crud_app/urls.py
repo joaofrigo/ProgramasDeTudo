44e524ffbbc59193e85d5d6a5525ff98bc722c87
joão marcos <joaofrigo1@gmail.com>
2025-07-11 02:08:09 -0300

    remoção de código redundante + ajuste de nome

--- TrabalhosGeneralizados/crud desempenho simples/projeto_crud/crud_app/urls.py
+++
@@ -1,8 +0,0 @@
-# meu_projeto/projeto_crud/urls.py
-from django.urls import path
-from .views import home_view
-
-urlpatterns = [
-    path('', home_view, name='nome_da_url_home'),
-    # ... outras URLs do seu aplicativo ...
-]
--- TrabalhosGeneralizados/crud desempenho simples/projeto_crud/crud_app/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def home_view(request):
-    return render(request, 'crud_app/templates/home.html')
-
